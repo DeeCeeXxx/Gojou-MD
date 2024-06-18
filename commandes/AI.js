90e77fa9cfbcbc7547e688a4e11caad65e5ea5e6
David Cyril <168988025+DeeCeeXxx@users.noreply.github.com>
2024-06-18 12:33:09 +0100

    Gojou-MD ⚡

--- commandes/AI.js
+++ commandes/AI.js
@@ -11,7 +11,7 @@ zokou({nomCom:"tkm",reaction:"📡",categorie:"TKM-Ai"},async(dest,zk,commandeOp
   const {repondre,ms,arg}=commandeOptions;
   
     if(!arg || !arg[0])
-    {return repondre("yes I'm listening to you.")}
+    {return repondre("`Yowaimo!!' yes I'm listening to you.")}
     //var quest = arg.join(' ');
   try{
     
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=182418&key=UQXAO1yyrPLRnhf6&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '┃powered by ⬡〘TKM md〙⬡┃';
+      let caption = '┃powered by ⬡〘Gojou-MD〙⬡┃';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
