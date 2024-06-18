959484f352dacadc91761a1c5edc3ea6a3f81cd7
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-06-18 05:51:10 +0200

    Update groupe.js

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -32,7 +32,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        ⬡ ┃TKM bot┃ ⬡
+        *Gojou-md*
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
